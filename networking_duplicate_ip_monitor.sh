739929f5b4dec7e42ecaf4ce4ccc87d229bda9bc
jserlan <20057621+jserlan@users.noreply.github.com>
2020-02-07 15:44:39 +0100

    Update networking_duplicate_ip_monitor.sh

--- networking_duplicate_ip_monitor.sh
+++ networking_duplicate_ip_monitor.sh
@@ -3,12 +3,24 @@ NETIFACE=eth0
 MAILFROM=mail@exemple.com
 MAILTO=mail@exemple.com
 MAILSUBJECT="DUPLICATE IPs Found"
-arp_scan()
-{
-arp-scan --interface=$NETIFACE --localnet > /tmp/arp-scan_$NETIFACE.txt
-DUPLICATEIP=$(cat /tmp/arp-scan_$NETIFACE.txt | grep "DUP" | awk '{print $1}')
+DUPLICATEIPFILE="/tmp/arp-scan_$NETIFACE.txt"
 
-for i in $DUPLICATEIP
+if [ -f $DUPLICATEIPFILE ]
+then
+        rm $DUPLICATEIPFILE
+fi
+
+arp-scan --interface=$NETIFACE --localnet > $DUPLICATEIPFILE
+ERRORCODE=$?
+if [ ! $ERRORCODE -eq 0 ]
+then
+        echo "arp-scan exited on error $?"
+        exit 1
+fi
+
+arp_scan_output_html()
+{
+for i in $(cat $DUPLICATEIPFILE | grep "DUP" | awk '{print $1}')
 do
         echo "<p><b>Duplicate IPs found for $i :</b>"
         echo "<pre>$(cat /tmp/arp-scan_$NETIFACE.txt | grep $i)</pre>"
@@ -16,12 +28,12 @@ do
 done
 }
 
-if [ -n DUPLICATEIP ]
+if [ ! -s $DUPLICATEIPFILE ]
 then
         echo "No duplicate IPs found"
         exit 0
 else
-        arp_scan | mail \
+        arp_scan_output_html | mail \
         -a "From: $MAILFROM" \
         -a "MIME-Version: 1.0" \
         -a "Content-Type: text/html" \
